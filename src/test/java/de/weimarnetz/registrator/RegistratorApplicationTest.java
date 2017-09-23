f36025bbc8d0d642512061d5ce2a91a3467380a1
Andreas Bräu <andreas.braeu@idealo.de>
2017-09-23 16:40:15 +0200

    rename integration test

--- src/test/java/de/weimarnetz/registrator/RegistratorApplicationTest.java
+++ src/test/java/de/weimarnetz/registrator/RegistratorApplicationTest.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class RegistratorApplicationTests {
+public class RegistratorApplicationTest {
 
 	@Test
 	public void contextLoads() {
