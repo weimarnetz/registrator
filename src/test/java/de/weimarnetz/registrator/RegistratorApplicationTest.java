2582570fbd5a2b02cb67534856cc17dd41c90573
Andreas Bräu <ab@andi95.de>
2017-10-07 22:03:14 +0200

    change test name

--- src/test/java/de/weimarnetz/registrator/RegistratorApplicationTest.java
+++ src/test/java/de/weimarnetz/registrator/RegistratorApplicationTest.java
@@ -1,16 +1,16 @@
 package de.weimarnetz.registrator;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import javax.inject.Inject;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.inject.Inject;
+
 import de.weimarnetz.registrator.services.NodeNumberService;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = RegistratorApplication.class)
@@ -20,8 +20,8 @@ public class RegistratorApplicationTest {
 	private ApplicationContext applicationContext;
 
 	@Test
-	public void beansloaded() {
-		assertThat(applicationContext.getBean(NodeNumberService.class)).isNotNull();
+    public void contextLoads() {
+        assertThat(applicationContext.getBean(NodeNumberService.class)).isNotNull();
 	}
 
 
